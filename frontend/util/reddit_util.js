2ac7323bafb61f0b896f2cab99e72da49f1bd12c
Betty <b.lam4392@gmail.com>
2018-03-12 03:09:57 -0700

    Refactor unused dependancies and code

--- frontend/util/reddit_util.js
+++ frontend/util/reddit_util.js
@@ -1,23 +1,7 @@
-// import fetchJsonp from 'fetch-jsonp';
-
 export default class RedditAPI {
   static async getPosts(subreddit) {
     try {
       let response = await fetch(`https://www.reddit.com/r/${subreddit}.json`, {
-        method: 'GET',
-        headers: {
-          'Accept': 'application/json',
-        }
-      });
-      return response.json();
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
-  static async getRecommendedSubreddits() {
-    try {
-      let response = await fetch("https://www.reddit.com/subreddits/popular.json", {
         method: 'GET',
         headers: {
           'Accept': 'application/json',
