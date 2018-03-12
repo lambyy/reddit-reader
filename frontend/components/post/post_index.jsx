dc43807ab090158b983c30837b03754abaf9e21b
Betty <b.lam4392@gmail.com>
2018-03-12 04:35:33 -0700

    Posts from /r/news show by default if not subscribed to any other subreddits

--- frontend/components/post/post_index.jsx
+++ frontend/components/post/post_index.jsx
@@ -6,17 +6,17 @@ class PostIndex extends React.Component {
     this.props.requestPosts("news");
   }
 
+  componentWillReceiveProps(nextProps) {
+    if(nextProps.posts.length === 0) {
+      this.props.requestPosts("news");
+    }
+  }
+
   render() {
     const { posts } = this.props;
-    let postsList = <div className="none">
-                      No posts found. Subscribe to a subreddit to view!
-                    </div>;
-
-    if (posts.length !== 0) {
-      postsList = posts.map( post =>
-        <Post key={post.data.id} post={post.data} />
-      );
-    }
+    let postsList  = posts.map( post =>
+      <Post key={post.data.id} post={post.data} />
+    );
 
     return (
       <div className="posts">
